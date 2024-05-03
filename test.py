6049af68ae6115203bb114bbcc72a815ddb77b49
Dheeman Das <84588268+Reliable-Royalty-29@users.noreply.github.com>
2024-05-03 16:24:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,4 +11,4 @@ if cuisine:
     menu_items = response['menu_items'].strip().split(",")
     st.write("**Menu Items**")
     for item in menu_items:
-        st.text("-",item)
+        st.write("-", item)
