be7ce207e8554e6f527d0e8a3a7223dd96087287
Dheeman Das <84588268+Reliable-Royalty-29@users.noreply.github.com>
2024-05-03 16:42:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 import streamlit as st
-import langchain_helper
+from langchain_helper import generate_restaurant_name_and_items
 
-st.title("Restaurant Name Generator")
+def main():
+    st.title("Restaurant Name Generator")
 
-cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican", "Arabic", "American"))
+    cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican", "Arabic", "American"))
 
-if cuisine:
-    response = langchain_helper.generate_restaurant_name_and_items(cuisine)
-    st.header(response['restaurant_name'].strip())
-    menu_items = response['menu_items'].strip().split(",")
-    st.write("**Menu Items**")
-    for item in menu_items:
-        st.write("-", item)
+    if cuisine:
+        response = generate_restaurant_name_and_items(cuisine)
+        st.header(response['restaurant_name'].strip())
+        menu_items = response['menu_items'].strip().split(",")
+        st.subheader("Menu Items")
+        for item in menu_items:
+            st.write("-", item.strip())
+
+if __name__ == "__main__":
+    main()
